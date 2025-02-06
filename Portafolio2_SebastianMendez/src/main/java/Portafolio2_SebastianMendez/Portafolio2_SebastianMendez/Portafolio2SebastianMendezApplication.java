70ffc4e11bd6d1ee25aef43518d281d6ce4ef73d
Sebastián Roberto Méndez Artavia <smendez40262@ufide.ac.cr>
2025-02-05 18:29:22 -0600

    s
    s

---
+++ Portafolio2_SebastianMendez/src/main/java/Portafolio2_SebastianMendez/Portafolio2_SebastianMendez/Portafolio2SebastianMendezApplication.java
@@ -0,0 +1,13 @@
+package Portafolio2_SebastianMendez.Portafolio2_SebastianMendez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portafolio2SebastianMendezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portafolio2SebastianMendezApplication.class, args);
+	}
+
+}
---
+++ Portafolio2_SebastianMendez/src/test/java/Portafolio2_SebastianMendez/Portafolio2_SebastianMendez/Portafolio2SebastianMendezApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio2_SebastianMendez.Portafolio2_SebastianMendez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portafolio2SebastianMendezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
